50a84186d5bd20a815aae575f4695c1999756ba2
freecoss <abderrahimfarahi99@gmail.com>
2026-05-05 14:37:22 +0100

    feat: configure standalone application bootstrap with HttpClient and router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ebanking-angular-front');
+  title = 'ebanking-angular-front';
 }
+
